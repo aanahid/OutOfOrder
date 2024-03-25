e1c95d2b0532e3bf1cb9c6ba3c2299683d8a7ab0
Alondra Alfaro Bonilla <107895705+aanahid@users.noreply.github.com>
2024-03-25 19:08:49 -0400

    Tried

--- interaction/InteractionManager.gd
+++ interaction/InteractionManager.gd
@@ -4,18 +4,20 @@ extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var label = $Label
 
-const base_text = 'E to '
+const base_text = "[E] to "
 
 var active_areas = []
 var can_interact = true
 
 func register_area(area: InteractionArea):
 		active_areas.push_back(area)
+		print("area registered")
 		
 func unregister_area(area: InteractionArea):
 		var index = active_areas.find(area)
 		if index != -1:
 			active_areas.remove_at(index)
+			print("area unregistered")
 
 func _process(delta):
 	if active_areas.size() > 0 && can_interact:
--- interaction/interaction area/interaction_area.gd
+++ interaction/interaction area/interaction_area.gd
@@ -6,10 +6,11 @@ class_name InteractionArea
 var interact: Callable = func():
 	pass
 
-
 func _on_body_entered(body):
 	InteractionManager.register_area(self)
+	print("entered")
 
 
 func _on_body_exited(body):
 	InteractionManager.unregister_area(self)
+	print("exited")
